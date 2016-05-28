1e340006b40cedef553ff557d3a5e28624576ea2
Sam Yesipov <semmy071997@gmail.com>
2016-05-28 13:52:16 +0300

    First commit

---
+++ mysite/settings.py
@@ -0,0 +1,20 @@
+import os
+TIME_ZONE = 'Europe/Kiev'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'blog',
+)
